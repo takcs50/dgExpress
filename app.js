cfef1a594f1cfd1cc3acd3db23924ac464ec8159
Tariq Khan <tariqanwarkhan@users.noreply.github.com>
2016-01-28 01:09:47 -0800

    Modified file for openshift deployment

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 // openshift server configuration
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 // To use EJS Template Engine with Express
 app.set('view engine', 'ejs');
@@ -38,5 +38,5 @@ app.get('*', function(req, res) {
 
 // for deployment to openshift
 server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+  console.log( "Listening on " + server_ip_address + ", server_port " + port );
 });
